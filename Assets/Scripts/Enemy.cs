267f1b3bfdeabc87a670ad685f87095c46175292
Craig Wechtler <cwechtler@gmail.com>
2022-01-23 12:46:21 -0600

    Enemy Spawners

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 	[SerializeField] private skillElementType skillElementType;
+	[SerializeField] private float health = 50f;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -14,6 +16,23 @@ public class Enemy : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		
+
+	}
+
+	public void reduceHealth(float damage) {
+		health -= damage;
+
+		Debug.Log("Damage: " + damage);
+		if (health <= 0) {
+			Destroy(gameObject);
+		}
+
+		Debug.Log("health: " + health);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.CompareTag("Projectile"))
+			reduceHealth(collision.GetComponent<Projectile>().GetDamage());
 	}
 }
